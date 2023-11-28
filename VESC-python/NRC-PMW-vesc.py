536cd043f8ba755237d40a15c1c8490ff7956f56
Chimdera Benson Ezeani <99694923+Chidelz@users.noreply.github.com>
2023-11-28 14:08:10 -0500

    Add files via upload

--- VESC-python/NRC-PMW-vesc.py
+++ VESC-python/NRC-PMW-vesc.py
@@ -7,85 +7,129 @@
 
   To set an rpm, just type a number and hit enter.  A separate thread manages this
   but since the data is streaming on the screen, you won't see what you type.
+
+  
 '''
 
-
-
 import pyvesc
-from pyvesc import GetValues, SetDutyCycle, SetAlive
+from pyvesc import GetFirmwareVersion, \
+    GetValues, SetRPM, SetCurrent, \
+    SetRotorPositionMode, GetRotorPosition, \
+    SetDutyCycle, SetServoPosition, SetPosition, \
+    GetRotorPositionCumulative, \
+    SetCurrentGetPosCumulative, \
+    SetPositionCumulative, SetTerminalCommand, \
+    GetPrint, GetConfig, SetConfig, \
+    SetAlive, GetDetectEncoder 
 import serial
+import os
+#import math
 import time
 import threading
 
+#import struct
+
+
+# Set your serial port here (either /dev/ttyX or COMX)
+#serialport = '/dev/cu.usbmodem3011'
+# serialport = '/dev/ttyACM0'
 serialport = 'COM3'
+
 
 print("port " + serialport)
 
-desired_pwm = 0.0
-pwm_lock = threading.Lock()
+desired_rpm = 0
 
-def getKeyPWM():
-    global desired_pwm
-    while True:
-        try:
-            new_pwm = float(input("Desired PWM? "))
-            with pwm_lock:
-                desired_pwm = new_pwm
-        except ValueError:
-            print("Invalid input. Please enter a valid floating-point number.")
+def getKeyRPM():
+  global desired_rpm
+  while True:
+    try:
+        user_input = input("Rpm?")
+        desired_rpm = float(user_input)
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
 
-def do_VESC():
-    global desired_pwm
+    
+def ctof(value):
+  return (value * 9.0/5) + 32
+
+def do_VESC():    
+    global desired_rpm
     last_sample_time = 0
-    inbuf = b''
-    old_pwm = -1.0
+    inbuf = b''    
+    oldRPM = -1
     with serial.Serial(serialport, baudrate=115200, timeout=0) as ser:
         try:
             ser.flushInput()
             ser.flushOutput()
-
-            ser.write(pyvesc.encode(SetAlive))
-
+                                                                     
+            #sendConfig(ser)
+            
+            ser.write(pyvesc.encode(SetTerminalCommand('ping')))                        
+                                                                                                 
             while True:
-                ser.write(pyvesc.encode(SetAlive))
-                ser.write(pyvesc.encode_request(GetValues))
+                ser.write(pyvesc.encode(SetAlive))                                                                    
+                ser.write(pyvesc.encode_request(GetValues))                                                    
+                
 
                 time.sleep(0.01)
-
-                while ser.in_waiting > 0:
-                    inbuf += ser.read(ser.in_waiting)
-
-                if len(inbuf) == 0:
-                    continue
-
-                (response, consumed) = pyvesc.decode(inbuf)
-
-                if consumed == 0:
-                    continue
-
-                inbuf = inbuf[consumed:]
-
-                if isinstance(response, GetValues):
+                
+                                                
+                while (True):
+                  # Check if there is enough data back for a measurement
+                  while ser.in_waiting > 0:                   
+                    inbuf += ser.read(ser.in_waiting)                                   
+                  if len(inbuf) == 0: break                 
+                  
+                  (response, consumed) = pyvesc.decode(inbuf)
+                  if consumed == 0: break           # Need more data to get a full message                     
+                  inbuf = inbuf[consumed:]                      
+                  
+                  # Only if we got a values packet do we report and update rpm                           
+                  if isinstance(response, GetValues):
+                    '''print(f" Fet temp C: {ctof(response.temp_fet_filtered):0.3f} " + 
+                      f" motor temp C: {ctof(response.temp_motor_filtered):0.3f} " +
+                      f" In volt: {response.input_voltage:0.2f}" + 
+                      f" motor curr: {response.avg_motor_current:0.3f}" + 
+                      f" avg input current: {response.avg_input_current:0.3f}" +
+                      f" Duty: {response.duty_cycle_now:0.3f}" + 
+                      #" Watt Hours:" + str(response.watt_hours) + 
+                      #" Watt Hours Charged:" + str(response.watt_hours_charged) + 
+                      #" amp Hours:" + str(response.amp_hours) + 
+                      #" amp Hours Charged:" + str(response.amp_hours_charged) + 
+                       "")'''
+                    # Loop through all the properies of the reponse and print
                     for field in vars(response):
-                        if field[0] == "_":
-                            continue
-                        print(f"{field}: {vars(response)[field]}")
+                      if field[0] == "_":
+                        continue
+                      print(f"{field}: {vars(response)[field]}")
+                    
+                    #print(dir(response))
 
-                    with pwm_lock:
-                        if old_pwm != desired_pwm:
-                            print(f"Desired PWM: {desired_pwm}")
-                            ser.write(pyvesc.encode(SetDutyCycle(desired_pwm)))
-                            old_pwm = desired_pwm
+                    if oldRPM != desired_rpm:
+                        print(f"Desired Steering Movement: {desired_rpm}")
+                        # Adjust the scale based on your servo and desired range
+                        servo_pos = desired_rpm / 100.0  
+                        ser.write(pyvesc.encode(SetServoPosition(servo_pos)))
+                        oldRPM = desired_rpm   
+                    # Report refesh rate
+                    print(f"VESC refresh rate: {1.0/(time.time() - last_sample_time):0.1f}")   
+                    last_sample_time = time.time()   
+                    print()   
 
-                    print(f"VESC refresh rate: {1.0 / (time.time() - last_sample_time):0.1f}")
-                    last_sample_time = time.time()
-                    print()
+
+                  
 
         except KeyboardInterrupt:
-            print("Turning off the VESC...")
-            with pwm_lock:
-                ser.write(pyvesc.encode(SetDutyCycle(0.0)))
+            # Turn Off the VESC
+            print ("turning off the VESC...")
+            #ser.write(pyvesc.encode(SetCurrent(0)))            
+            ser.flushOutput()
+            ser.flushInput()
+            ser.close()
+            
 
 if __name__ == "__main__":
-    threading.Thread(target=getKeyPWM).start()
+    # Spawn a thread to listen to the keyboard
+    threading.Thread(target=getKeyRPM).start()  
     do_VESC()
