65831bebd5a1f8f99d98e2685b64dc7647084bbf
Joao <araujosilvaj@uindy.edu>
2023-11-27 20:27:12 -0500

    not capture the same image feature

--- img-capture/functional/camera3.py
+++ img-capture/functional/camera3.py
@@ -2,26 +2,40 @@ import cv2
 import os
 import time
 
-directory = '/home/robot/Pictures/left'
-prev_frame = None
-i = 0
+directory = '/home/robot/Pictures/right'
 
 c = cv2.VideoCapture(2)
 c.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+
 c.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 c.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 os.chdir(directory)
 
-
+i = 0
+ret, prev_frame = c.read()
 
 while True:
 	ret, frame = c.read()
-	if prev_frame is None or not (frame == prev_frame).all():		
+	#set images to same color
+	prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+	current_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	#absolute difference between the two frames
+	abs_diff = cv2.absdiff(prev_gray, current_gray)
+
+	#set a threshold for the absolute difference
+	threshold = 30
+	_, thresholded_diff = cv2.threshold(abs_diff, threshold, 255, cv2.THRESH_BINARY)
+
+	if cv2.countNonZero(thresholded_diff) > 10000:
+		print(f"different frame --> {cv2.countNonZero(thresholded_diff)}")
 		name = os.popen("echo -n `date +\"%Y-%m-%d-%H:%M:%S%N\"`").read()
-		filename = str(name) + "-L" + str(i) + ".jpg"
+		filename = str(name) + "-" + str(i) + ".jpg"
 		cv2.imwrite(filename, frame)
 		print(filename)
-		time.sleep(0.5)
 		i = i + 1
-	prev_frame = frame.copy()
+	else:
+		print(f'Same frame -->{cv2.countNonZero(thresholded_diff)}')
+	time.sleep(0.5)
+	prev_frame = frame	
