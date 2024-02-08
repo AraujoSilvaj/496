bd691e10d10bc41f4f09de9bbda6579210fe1690
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-02-08 11:35:20 -1000

    Updating get_box_height to compute distances and angles, assuming linear fit for distance for now

--- ros_ws/src/bounding_boxes/bounding_boxes/get_box_height.py
+++ ros_ws/src/bounding_boxes/bounding_boxes/get_box_height.py
@@ -9,6 +9,9 @@ total_x_avg = []
 final_h_avg = []
 final_x_avg = []
 
+distance_list = []
+angle_list = []
+
 num_readings = 100
 laser_frequency = 40
 
@@ -24,11 +27,22 @@ class BoxSubscriber(Node):
 		#self.subscription
 		
 	def listener_callback(self, msg):
+		
+		global distance_list, angle_list
+		
+		data = list(msg.data)
+		distances = pixel_to_distance(data)
+		angles = x_to_rads(data)
+		print('distances: ', distances)
+		print('angles: ', angles)
+		'''
 		global total_h_avg, total_x_avg
 		global final_h_avg, final_x_avg
 		data = list(msg.data)
 		x_avg, h_avg = get_averages(data)
 		print("height avg: ", h_avg)
+		'''
+		
 		'''
 		if (len(total_h_avg) < 10):
 			total_h_avg.append(h_avg)
@@ -58,10 +72,10 @@ class BoxSubscriber(Node):
 				print("Final x average:", x)
 				final_x_avg = []'''
 
-#TODO get actual angle of vision
-# convert actual angle to rads
+#TODO get actual angle of vision = 78 degrees = +-39
+# convert actual angle to rads +-0.681 rads
 # 0 is at center of camera
-# convert x coord to 
+# convert x coord to rads
 # convert bb height to distance
 # update num_readings and laser_frequency
 # range_min,max units -> 
@@ -84,12 +98,12 @@ class LaserScanPublisher(Node):
 		scan = LaserScan()
 		scan.header.stamp = current_time
 		scan.header.frame_id = 'laser_frame'
-		scan.angle_min = -1.57
-		scan.angle_max = 1.57
+		scan.angle_min = -0.681
+		scan.angle_max = 0.681
 		scan.angle_increment = 3.14/num_readings
 		scan.time_increment = (1.0 / laser_frequency) / num_readings
 		scan.range_min = 0.0
-		scan.range_max = 100.0
+		scan.range_max = 100
 		
 		scan.ranges = []
 		scan.intensities = []
@@ -114,9 +128,6 @@ def main(args=None):
 	#rclpy.spin(box_subscriber)
 	rclpy.spin(laser_scan_publisher)
 	
-	
-	
-	
 	box_subscriber.destroy_node()
 	laser_scan_publisher.destroy_node()
 	rclpy.shutdown()
@@ -130,15 +141,42 @@ def get_averages(lst):
 		h_avg = sum(heights) / len(heights)
 	
 	else:
-		return None
+		return []
 	
 	if x_coords:
-		x_avg = sum(x_coords) / len(x_coords)
-		
-	
+		x_avg = sum(x_coords) / len(x_coords)		
 		
 	return x_avg, h_avg	
 	
+
+#update with tan function
+#need to find tan function constant
+#see sticky note
+
+def pixel_to_distance(lst):
+	heights = lst[2::3]
+	if heights:
+		distance_list = [calculate_distance(height) for height in heights]
+		print(distance_list)
+		return distance_list
+	else
+		return []
+
+def calculate_distance(h):
+	return -.014*h + 0.34
+	
+def x_to_rads(lst):
+	x_coords = lst[0::3]
+	if x_coords:
+		angle_list = [calculate_angle(x) for x in x_coords]
+		print(angle_list)
+		return(angle_list)
+	else
+		return []
+		
+def calculate_angle(x):
+	return 1.3628*x - 0.681
+
 if __name__ == '__main__':
 	main()
 	
