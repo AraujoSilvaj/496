51e0089bb222bda9ca27a1dae15a25e197b9b4d8
avc <avc.robot2024@gmail.com>
2024-02-13 14:18:01 -0500

    update imu pkg

--- ros_ws/src/imu/imu/imu_pub.py
+++ ros_ws/src/imu/imu/imu_pub.py
@@ -3,9 +3,11 @@ from rclpy.node import Node
 import serial 
 
 from sensor_msgs.msg import Imu
+
 class ImuPublisher(Node): 
     def __init__(self): 
-        super.publisher_ = self.create_publisher(Imu, 'imu-pub', 10)
+        super().__init('imu_publisher')
+        self.publisher_ = self.create_publisher(Imu, 'imu-pub', 10)
         timer_period = 0.5
         self.timer = self.create_timer(timer_period, self.timer_callback)
         
@@ -14,7 +16,7 @@ class ImuPublisher(Node):
         imu = Serial('/dev/ttyIMU', 9600)
         msg.data = imu.readline()
         self.publisher_.publish(msg)
-        self.getlogger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
 
 def main(args=None): 
 	rclpy.init(args=args)
