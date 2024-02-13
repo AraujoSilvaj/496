45ab09145232bb48d803fb9a634626595fc1545a
NewJetson <avc.robot2024@gmail.com>
2024-02-13 13:17:17 -0500

    updated rand.py to test publishing ackermann msg

--- ros_ws/src/rand_num_publisher/rand_num_publisher/rand.py
+++ ros_ws/src/rand_num_publisher/rand_num_publisher/rand.py
@@ -3,22 +3,27 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from ackermann_msgs.msg import AckermannDriveStamped
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'rand', 10)
+        self.publisher = self.create_publisher(AckermannDriveStamped, 'ackermann_cmd', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         #self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Some random nums: '
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        msg = AckermannDriveStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'base_link'
+        msg.drive.steering_angle = 0.5  # Set your desired steering angle (in radians)
+        msg.drive.speed = 1.0  # Set your desired linear velocity (in m/s)
+        msg.drive.acceleration = 0.5  # Set your desired acceleration (in m/s^2)
+        self.publisher.publish(msg)
+        self.get_logger().info('Publishing Ackermann Drive Command: "%s"' % msg)
         #self.i += 1
 
 
