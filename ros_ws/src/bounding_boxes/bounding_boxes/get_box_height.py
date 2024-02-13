3b344452f1f36e86e90809f02116292966d43555
NewJetson <avc.robot2024@gmail.com>
2024-02-13 09:47:10 -0500

    Updated distance computation

--- ros_ws/src/bounding_boxes/bounding_boxes/get_box_height.py
+++ ros_ws/src/bounding_boxes/bounding_boxes/get_box_height.py
@@ -35,48 +35,11 @@ class BoxSubscriber(Node):
 		angles = x_to_rads(data)
 		print('distances: ', distances)
 		print('angles: ', angles)
-		'''
-		global total_h_avg, total_x_avg
-		global final_h_avg, final_x_avg
-		data = list(msg.data)
-		x_avg, h_avg = get_averages(data)
-		print("height avg: ", h_avg)
-		'''
 		
-		'''
-		if (len(total_h_avg) < 10):
-			total_h_avg.append(h_avg)
-			print(total_h_avg)	
-		
-		if (len(total_h_avg) == 10):
-			height_avg = sum(total_h_avg)/len(total_h_avg)
-			print("Total Height average:", height_avg)
-			total_h_avg = []
-			final_h_avg.append(height_avg)
-			print(final_h_avg)
-			if(len(final_h_avg) == 10):
-				h = sum(final_h_avg)/len(final_h_avg)
-				print("Final height average:", h)
-				final_h_avg = []
-		
-		if (len(total_x_avg) < 10):
-			total_x_avg.append(x_avg)	
-		
-		if (len(total_x_avg) == 10):
-			x_avg = sum(total_x_avg)/len(total_x_avg)
-			#print("Total X coord average:", x_avg)
-			total_x_avg = []
-			final_x_avg.append(x_avg)
-			if(len(final_x_avg) == 10):
-				x = sum(final_x_avg)/len(final_x_avg)
-				print("Final x average:", x)
-				final_x_avg = []'''
 
 #TODO get actual angle of vision = 78 degrees = +-39
 # convert actual angle to rads +-0.681 rads
 # 0 is at center of camera
-# convert x coord to rads
-# convert bb height to distance
 # update num_readings and laser_frequency
 # range_min,max units -> 
 # intensities = 
@@ -147,11 +110,6 @@ def get_averages(lst):
 		x_avg = sum(x_coords) / len(x_coords)		
 		
 	return x_avg, h_avg	
-	
-
-#update with tan function
-#need to find tan function constant
-#see sticky note
 
 def pixel_to_distance(lst):
 	heights = lst[2::3]
@@ -163,7 +121,7 @@ def pixel_to_distance(lst):
 		return []
 
 def calculate_distance(h):
-	return -.014*h + 0.34
+	return 0.6 / np.tan(h * 1.02 / 2)
 	
 def x_to_rads(lst):
 	x_coords = lst[0::3]
