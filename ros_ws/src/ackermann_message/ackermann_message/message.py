c69ffe89e5ff1db7a7731b9669a5c4ffdd04c8c0
avc <avc.robot2024@gmail.com>
2024-02-19 18:40:39 -0500

    recreated ackermann_msgs as ackermann_message because it had the same name as one of import statements which cause an error.

---
+++ ros_ws/src/ackermann_message/ackermann_message/message.py
@@ -0,0 +1,54 @@
+import rclpy
+import time 
+#import random
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from ackermann_msgs.msg import AckermannDriveStamped
+
+class AckermannPublisher(Node):
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher = self.create_publisher(AckermannDriveStamped, 'ackermann_cmd', 10)
+        timer_period = 0.5 # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+    
+    def timer_callback(self):
+        msg = AckermannDriveStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'base_link'
+        
+        if (self.i < 30): #driving straight
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 0.25
+            msg.drive.acceleration = 0.5
+            print("Drive Forward")
+        #elif (self.i < 30): #turning right
+            #msg.drive.steering_angle = -0.26 
+            #msg.drive.speed = 0.25  
+            #msg.drive.acceleration = 0.5
+            #print("Drive Forward")
+        
+        self.publisher.publish(msg)
+        #self.get_logger().info('Publishing Ackermann Drive Command: "%s"' % msg)
+        self.i += 1
+       
+def main(args=None):
+    rclpy.init(args=args)
+
+    ackermann_publisher = AckermannPublisher()
+
+    rclpy.spin(ackermann_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
+        
