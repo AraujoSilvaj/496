c149c86aa526835d846619956e7682093f936f53
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-02-20 07:10:45 -1000

    Updating dependencies

--- ros_ws/src/vesc_voltage/setup.py
+++ ros_ws/src/vesc_voltage/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'rclpy',],
     zip_safe=True,
     maintainer='root',
     maintainer_email='root@todo.todo',
