4dd7a8f103410677c80ed54ac004b91fb76b07c6
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-02-20 07:52:48 -1000

    Added percentage publishing

--- ros_ws/src/vesc_voltage/vesc_voltage/vesc_voltage_publisher.py
+++ ros_ws/src/vesc_voltage/vesc_voltage/vesc_voltage_publisher.py
@@ -7,31 +7,29 @@ class VescVoltagePublisher(Node):
 
 	def __init__(self):
 		super().__init__('vesc_voltage_publisher')
-		
 		self.subscription = self.create_subscription(VescStateStamped, 'sensors/core',self.vesc_state_callback,10)
+		self.voltage_publisher = self.create_publisher(Float64, 'input_voltage', 10)
+		self.battery_percentage_publisher = self.create_publisher(Float64,'battery_percentage',10)
 		
-		#self.vesc_voltage_publisher = self.create_publisher(VescStateStamped, 'vesc_voltage', 10)
+		self.min_voltage = 11.1
+		self.max_voltage = 12.4
 		
-		self.publisher = self.create_publisher(Float64, 'input_voltage', 10)
-		
-		#self.timer = self.create_timer(1.0, self.publish_voltage)
-		
-	'''def publish_voltage(self):
-		msg = VescStateStamped()
-		
-		msg.state.voltage_input = 48.0 # replace with actual voltage
-		
-		self.vesc_voltage_publisher.publish(msg)
-		print(msg.state.voltage_input)
-		'''
 		
 	def vesc_state_callback(self,msg):
 		input_voltage = msg.state.voltage_input
-		print(f'Input Voltage: {input_voltage}V')
-		
 		voltage_msg = Float64()
 		voltage_msg.data = input_voltage
-		self.publisher.publish(voltage_msg)
+		self.voltage_publisher.publish(voltage_msg)
+		
+		# calculate battery percentage
+		battery_percentage = ((input_voltage - self.min_voltage) / (self.max_voltage - self.min_voltage) * 100
+		
+		battery_percentage_msg = Float64()
+		battery_percentage_msg.data = battery_percentage
+		self.battery_percentage_publisher.publish(battery_percentage_msg)
+		
+		print(f'Input Voltage: {input_voltage}V	Battery Percent: {battery_percentage}%')
+	
 		
 
 def main(args=None):
