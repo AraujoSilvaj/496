5497e38f9665dd476ac010d7c050ec1c0f3909ad
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-02-20 05:15:30 -1000

    Creating vesc voltage publisher

---
+++ ros_ws/src/vesc_voltage/vesc_voltage/vesc_voltage_publisher.py
@@ -0,0 +1,30 @@
+import rclpy
+from rclpy.node import Node
+from VescState.msg import voltage_input
+
+class VescVoltagePublisher(Node):
+
+	def __init__(self):
+		super().__init__('vesc_voltage_publisher')
+		
+		self.vesc_voltage_publisher = self.create_publisher(VescMsg, 'vesc_voltage', 10)
+		
+		self.timer = self.create_timer(1.0, self.publish_voltage)
+		
+	def publish_voltage(self):
+		msg = VescMsg()
+		
+		msg.voltage_input = 48 # replace with actual voltage
+		
+		self.vesc_voltage_publisher.publish(msg)
+		
+
+def main(args=None):
+	rclpy.init(args=args)
+	vesc_voltage_publisher = VescVoltagePublisher()
+	rclpy.spin(vesc_voltage_publisher)
+	vesc_voltage_publisher.destroy_node()
+	rclpy.shutdown()
+	
+if __name__ = '__main__':
+	main()
