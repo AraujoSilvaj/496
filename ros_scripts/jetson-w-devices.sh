5e3cfded31784425887487b37c72b33dccd2bbe8
Chimdera Benson Ezeani <99694923+Chidelz@users.noreply.github.com>
2024-02-20 01:42:39 -0500

    Updated to run all 3 devices

--- ros_scripts/jetson-w-devices.sh
+++ ros_scripts/jetson-w-devices.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker run -it --rm --net=host --device=/dev/ttyVESC -v /dev/shm:/dev/shm -v ~/496:/496 avc/devel
-docker run -it --rm --net=host --device=/dev/ttyGPS -v /dev/shm:/dev/shm -v ~/496:/496 avc/devel
+
+devices=("ttyVESC" "ttyGPS" "ttyIMU")
+
+for device in "${devices[@]}"; do
+    docker run -it --rm --net=host --device=/dev/$device -v /dev/shm:/dev/shm -v ~/496:/496 avc/devel
+done
