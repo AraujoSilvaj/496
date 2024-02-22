7caeeab31b55030a4e079a58797448656271d35f
avc <avc.robot2024@gmail.com>
2024-02-21 21:17:13 -0500

    edit follower

--- ros_ws/src/boxes_follower/boxes_follower/follow.py
+++ ros_ws/src/boxes_follower/boxes_follower/follow.py
@@ -8,9 +8,9 @@ from std_msgs.msg import String
 from std_msgs.msg import Float32MultiArray
 from ackermann_msgs.msg import AckermannDriveStamped
 
-alpha = 0.9
+alpha = 0.1
 beta = 1.0 - alpha
-
+set_dist = 2.0
 
 class AckermannPublisher(Node):
     def __init__(self):
@@ -33,12 +33,16 @@ class BoxSubscriber(Node):
         self.publisher = self.create_publisher(AckermannDriveStamped, 'ackermann_cmd', 10)
    
     def listener_callback(self, msg): 
-        global alpha, beta
+        global alpha, beta, set_dist
+        drive_msg = AckermannDriveStamped()
+        drive_msg.header.stamp = self.get_clock().now().to_msg()
+        drive_msg.header.frame_id = 'base_link'
         #self.get_logger().info('Fron Bounding_boxes: "%s"' % msg.data)
         
         curr_dist = self.curr
+        if not msg.data: 
+            return 
         new_dist, idx = pixel_to_max_distance(msg.data)
-        #print(new_height)   
         avg_dist = (alpha * curr_dist) + (beta * new_dist)
         self.curr = avg_dist
         
@@ -46,8 +50,23 @@ class BoxSubscriber(Node):
         x = x_coords[idx]
         
         print(avg_dist, new_dist, x)
-        	
-   	
+        
+        if (x != 0.5): 
+            if (x < 0.5): 
+                drive_msg.drive.steering_angle = 0.4
+            else: 
+                drive_msg.drive.steering_angle = -0.4
+        else: 
+            drive_msg.drive.steering_angle = 0.0
+        if (avg_dist > set_dist):
+            drive_msg.drive.speed = 0.25
+            drive_msg.drive.acceleration = 0.5
+        else:
+            drive_msg.drive.speed = 0.0
+            drive_msg.drive.acceleration = 0.0
+   	    
+        self.publisher.publish(drive_msg)
+        #print(drive_msg)
 def pixel_to_max_distance(lst):
     heights = lst[2::3]
     if heights:
