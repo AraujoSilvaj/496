0706b4a9b367f3fd36f9c19fd45d8c3b54c1e2c7
avc <avc.robot2024@gmail.com>
2024-02-21 20:03:58 -0500

    edited follower

--- ros_ws/src/boxes_follower/boxes_follower/follow.py
+++ ros_ws/src/boxes_follower/boxes_follower/follow.py
@@ -4,7 +4,7 @@ import time
 from rclpy.node import Node
 
 from std_msgs.msg import String
-from std_msgs
+from std_msgs.msg import Float32MultiArray
 from ackermann_msgs.msg import AckermannDriveStamped
 
 class AckermannPublisher(Node):
@@ -17,12 +17,42 @@ class AckermannPublisher(Node):
         
     def timer_callback(self):
         msg = AckermannDriveStamped()
-        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.stamp = self.tget_clock().now().to_msg()
         msg.header.frame_id = 'base_link'
         
 class BoxSubscriber(Node): 
     def __init__(self): 
-    self.seubscription = self.create_subscription(Float32MultiArray, boxes, self.listener_callback, 10)
+        super().__init__('box_subscriber')
+        self.subscription = self.create_subscription(Float32MultiArray, 'boxes', self.listener_callback, 10)
     
     def listener_callback(self, msg): 
-        self.get_logger().info('' % msg.data)
+        self.get_logger().info('Fron Bounding_boxes: "%s"' % msg.data)
+        
+   	    #curr_height = 
+        new_height = max(pixel_to_distance(msg.data))
+        print(new_height)   	
+   	
+def pixel_to_distance(lst):
+    heights = lst[2::3]
+    if heights:
+        distance_list = [calculate_distance(height) for height in heights]
+        print(distance_list)
+        return distance_list
+    else:
+        return []
+
+def calculate_distance(h):
+    return (0.18415 / np.tan(h * 1.02 / 2)) / 3.281 # convert feet to meters for SLAM4
+    
+def main(args=None):
+    rclpy.init(args=args)
+    
+    box_subscriber = BoxSubscriber()
+    
+    rclpy.spin(box_subscriber)
+    
+    box_subscriber.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
