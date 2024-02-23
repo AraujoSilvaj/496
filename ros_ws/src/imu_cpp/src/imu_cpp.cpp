71b4ec1559cdc84949c20b3586831183dd464bb7
Joao <cbajpas@gmail.com>
2024-02-23 15:51:35 -0500

    change file names

--- ros_ws/src/imu_cpp/src/imu_cpp.cpp
+++ ros_ws/src/imu_cpp/src/imu_cpp.cpp
@@ -1,10 +1,24 @@
-#include <cstdio>
+#include <Y3SpaceDriver.h>
 
-int main(int argc, char ** argv)
+
+int main(int argc, char **argv)
 {
-  (void) argc;
-  (void) argv;
+  	ros::init(argc, argv, "Y3SpaceDriver");
+  	ros::NodeHandle nh;
+  	ros::NodeHandle pnh("~");
 
-  printf("hello world imu_cpp package\n");
-  return 0;
+  	std::string port;
+  	int baudrate;
+  	int timeout;
+  	std::string mode;
+  	std::string frame;
+
+    pnh.param<std::string>("port", port, "/dev/ttyIMU"); 
+    pnh.param<int>("baudrate", baudrate, 115200);
+    pnh.param<int>("timeout", timeout, 60000);
+    pnh.param<std::string>("mode", mode, "relative");
+    pnh.param<std::string>("frame", frame, "imu_link");
+
+  	Y3SpaceDriver driver(nh, pnh, port, baudrate, timeout, mode, frame);
+  	driver.run();
 }
