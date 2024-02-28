10b437858b767ebe3bbedd28985268d56ab7fe48
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-02-28 12:10:05 -1000

    Full course hard coded, needs validating

--- ros_ws/src/ackermann_message/ackermann_message/message.py
+++ ros_ws/src/ackermann_message/ackermann_message/message.py
@@ -19,21 +19,52 @@ class AckermannPublisher(Node):
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = 'base_link'
         
-        if (self.i < 9): #driving straight
+        if (self.i < 9): # short driving straight 
             msg.drive.steering_angle = 0.0
             msg.drive.speed = 3.0
             msg.drive.acceleration = 0.1
             print("Drive Forward 6.75 meter (22 ft)")
-        elif (self.i < 15): 
+        elif (self.i < 15): #turning left
             msg.drive.steering_angle = 0.164
             msg.drive.speed = 3.0
             msg.drive.acceleration = 0.5
             print("Turn Left")
-        #elif (self.i < 30): #turning right
-            #msg.drive.steering_angle = -0.26 
-            #msg.drive.speed = 0.25  
-            #msg.drive.acceleration = 0.5
-            #print("Drive Forward")
+        elif (self.i < 35): #long drive straight
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0  
+            msg.drive.acceleration = 0.5
+            print("Drive Forward 15m (49.5 ft)")
+        elif (self.i < 41): #turning left
+            msg.drive.steering_angle = 0.164
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Turn Left")
+        elif (self.i < 56): #med drive straight
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0  
+            msg.drive.acceleration = 0.5
+            print("Drive Forward 11.25m (37 ft)")
+        elif (self.i < 62): #turning left
+            msg.drive.steering_angle = 0.164
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Turn Left")
+        elif (self.i < 82): #long drive straight
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0  
+            msg.drive.acceleration = 0.5
+            print("Drive Forward 15m (49.5 ft)")
+        elif (self.i < 88): #turning left
+            msg.drive.steering_angle = 0.164
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Turn Left")
+        elif (self.i < 92): # finish line
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.1
+            print("Drive Forward 3 meter (10 ft)")
+        
         
         self.publisher.publish(msg)
         #self.get_logger().info('Publishing Ackermann Drive Command: "%s"' % msg)
