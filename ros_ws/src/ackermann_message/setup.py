d9adfd4f382a86f33102d173b17c40d15227e13e
NewJetson <avc.robot2024@gmail.com>
2024-03-01 17:29:36 -0500

    deleted slam_toolbox and robot_localization repo

--- ros_ws/src/ackermann_message/setup.py
+++ ros_ws/src/ackermann_message/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = ackermann_message.message:main',
+            'message = ackermann_message.message:main',
         ],
     },
 )
