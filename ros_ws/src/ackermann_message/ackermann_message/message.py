fd030ea9107804382e04c5b60ec4a13a53b022d0
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-03-05 09:55:15 -1000

    added button subscription, works based on button state

--- ros_ws/src/ackermann_message/ackermann_message/message.py
+++ ros_ws/src/ackermann_message/ackermann_message/message.py
@@ -1,20 +1,34 @@
 import rclpy
 import time 
-#import random
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 from ackermann_msgs.msg import AckermannDriveStamped
+
+button = False
 
 class AckermannPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher = self.create_publisher(AckermannDriveStamped, 'ackermann_cmd', 10)
+        self.subscription = self.create_subscription(Bool, 'button_state', self.listener_callback, 10)
         timer_period = 0.25 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
     
+    def listener_callback(self, button_state):
+        global button
+        if button_state.data:
+            button = True
+        else:
+            self.i = 0
+            button = False
+            return
+    
     def timer_callback(self):
+        global button
+        print(button_state.data)
         msg = AckermannDriveStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = 'base_link'
@@ -23,7 +37,6 @@ class AckermannPublisher(Node):
         ## RECALIBRATE SERVO MIN MAX FOR FULL RANGE OF MOTION
         
         ## WIGGLE
-        
         if (2 < self.i < 6): # short driving straight 
             msg.drive.steering_angle = 0.0
             msg.drive.speed = 0.5
@@ -109,19 +122,11 @@ class AckermannPublisher(Node):
        
 def main(args=None):
     rclpy.init(args=args)
-
     ackermann_publisher = AckermannPublisher()
-
     rclpy.spin(ackermann_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    ackermann_publisher.destroy_node()
     rclpy.shutdown()
 
 
 if __name__ == '__main__':
     main()
-
-        
