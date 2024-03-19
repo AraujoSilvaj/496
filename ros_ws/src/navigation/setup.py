9fe2f51e0c53a9c44446e460cd4f82f1aad4ae32
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-03-19 14:21:31 -0400

    added a run command for navigation node

--- ros_ws/src/navigation/setup.py
+++ ros_ws/src/navigation/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'driving = navigation.odom_navigation:main',
+            
         ],
     },
 )
