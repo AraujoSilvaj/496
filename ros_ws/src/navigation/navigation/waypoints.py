3cb5266d808c2dc23f393157e421b61157011d7f
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-03-19 15:06:02 -0400

    working on waypoints publisher

---
+++ ros_ws/src/navigation/navigation/waypoints.py
@@ -0,0 +1,63 @@
+import rclpy 
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Bool
+
+class Waypoints(Node):
+    
+    def __init__(self):
+        super().__init__('waypoints')
+        self.subscriber = self.create_subscription(Odometry, 'odometry/filtered', self.pose_callback, 10)
+        self.waypoints = self.load_waypoints('/496/ros_ws/src/navigation/4_corners_waypoints_indoors.txt')
+        self.subscription = self.create_subscription(Bool, 'button_state', self.button_callback, 10)
+        self.waypoint_index = 0
+    
+    def button_callback(self, msg):
+        if  not button_state.data:
+            self.waypoint_index = 0
+        
+    def pose_callback(self, msg):
+        min_dist = 0.5 # minimum distance to waypoint in meters
+        
+        position = msg.pose.pose.position
+        orientation = msg.pose.pose.orientation
+        linear_velocity = msg.twist.twist.linear
+        angular_velocity = msg.twist.twist.angular
+        
+        # Compare current pose with the next waypoint
+        next_waypoint = self.waypoints[self.waypoint_index]
+        distance_to_waypoint = self.calculate_distance(position, next_waypoint.position)
+        
+        print("Waypoint index: ", self.waypoint_index)
+        
+        if distance_to_waypoint < min_dist:
+            self.waypoint_index += 1
+            if self.waypoint_index == len(self.waypoints):
+                #reached final waypoint (start line), stop the robot
+                return
+        
+    def load_waypoints(self, file_path):
+        waypoints = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                values = [float(value) for value in line.strip().split(',') if value]
+                x, y, z, w = values
+                quat = Quaternion(x=0.0, y=0.0, z=z, w=w)
+                waypoints.append(Pose(position=Point(x=x, y=y, z=0.0), orientation=quat))
+        return waypoints
+    
+    def calculate_distance(self, position1, position2):
+        dx = position1.x -position2.x
+        dy = position1.y - position2.y
+        return math.sqrt(dx**2 + dy**2)
+        
+def main(args=None):
+    rclpy.init(args=args)
+    waypoints = Waypoints()
+    rclpy.spin(waypoints)
+    waypoints.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
