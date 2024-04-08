d7c7cb0cdd8d79a6b60eef4a79820489abd1da37
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-04-08 08:17:36 -0400

    working on adding hoop to hardcoded driving

---
+++ ros_ws/src/ackermann_message/ackermann_message/drive_no_s.py
@@ -0,0 +1,122 @@
+import rclpy
+import time 
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from std_msgs.msg import Bool
+from ackermann_msgs.msg import AckermannDriveStamped
+
+#button = True
+
+class AckermannPublisher(Node):
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher = self.create_publisher(AckermannDriveStamped, 'ackermann_cmd', 10)
+        #self.subscription = self.create_subscription(Bool, 'button_state', self.listener_callback, 10)
+        timer_period = 0.25 # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+    
+    def listener_callback(self, button_state):
+        #global button
+        if  not button_state.data:
+            self.i = 0
+        #return
+
+    def timer_callback(self):
+        #global button
+        msg = AckermannDriveStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'base_link'
+        
+        ### TODO
+        ## RECALIBRATE SERVO MIN MAX FOR FULL RANGE OF MOTION
+        
+        if (self.i == 0):
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 0.0
+            msg.drive.acceleration = 0.0
+            print("STOP")
+        
+        ## Drive Course
+        elif (2 < self.i < 11): # DRIVE STRAIGHT
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.01
+            print("Drive Forward 6.75 meters (22 ft)")
+            
+        elif (11 <= self.i < 18): #turning left
+            msg.drive.steering_angle = 0.1725
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("First Left Turn")
+            
+        elif (18 <= self.i < 26): #Drive through Hoop
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Drive through hoop")
+            
+        elif (26 <= self.i < 30): #turning right after hoop
+            msg.drive.steering_angle = -0.1725
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Turning Right after hoop")
+            
+        elif (30 <= self.i < 36): #Driving straight after hoop
+            msg.drive.steering_angle = 0.1725
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Driving straight after hoop")
+        """    
+        elif (37 <= self.i < 57): #long drive straight
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0  
+            msg.drive.acceleration = 0.5
+            print("Drive Forward 15m (49.5 ft)")
+        elif ( 57 <= self.i < 63): #turning left
+            msg.drive.steering_angle = 0.175
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Turn Left")
+        elif (63 <= self.i < 78): #med drive straight
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0  
+            msg.drive.acceleration = 0.5
+            print("Drive Forward 11.25m (37 ft)")
+        elif (78 <= self.i < 84): #turning left
+            msg.drive.steering_angle = 0.175
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Turn Left")
+        elif (84 <= self.i < 104): #long drive straight
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0  
+            msg.drive.acceleration = 0.5
+            print("Drive Forward 15m (49.5 ft)")
+        elif (104 <= self.i < 111): #turning left
+            msg.drive.steering_angle = 0.1705
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.5
+            print("Turn Left")
+        elif (111 <= self.i < 122): # finish line
+            msg.drive.steering_angle = 0.0
+            msg.drive.speed = 3.0
+            msg.drive.acceleration = 0.1
+            print("Drive Forward 3 meter (10 ft)")
+            print("YOU MADE IT TO THE FINISH LINE")
+        """
+        self.publisher.publish(msg)
+        #self.get_logger().info('Publishing Ackermann Drive Command: "%s"' % msg)
+        self.i += 1
+       
+def main(args=None):
+    rclpy.init(args=args)
+    ackermann_publisher = AckermannPublisher()
+    rclpy.spin(ackermann_publisher)
+    ackermann_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
