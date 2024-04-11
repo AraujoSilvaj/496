5c0a592110c5ea2032ec119a2c401a7b226ea734
vboxuser <vboxuser@nrc.myguest.virtualbox.org>
2024-04-11 13:37:04 -0400

    need to fix orientation discontinuity

--- ros_ws/src/ackermann_message/ackermann_message/imu_turning.py
+++ ros_ws/src/ackermann_message/ackermann_message/imu_turning.py
@@ -18,7 +18,7 @@ class AckermannPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
         self.current_yaw = 0.0
-        self.target_yaw = 0.0
+        self.target_yaw = 1.57
         self.turning = False
         self.map_index = 0
     
@@ -29,32 +29,40 @@ class AckermannPublisher(Node):
     def imu_callback(self, imu_msg):
         #Extract the yaw angle from the imu message
         quaternion = imu_msg. orientation
-        self.current_yaw = 2 * math.atan2(quaternion.z, quaternion.w)
+        yaw = 2 * math.atan2(quaternion.z, quaternion.w)
         
-        # If we're not currently turning, set the target yaw to 90 degrees
-        if not self.turning:
-            self.target_yaw = self.current_yaw + 157 # 90 degrees in radians
-            
+        # Normalize the yaw angle to be between -pi and pi
+        self.current_yaw = (yaw + 2 * math.pi) % (2 * math.pi)
+        if self.current_yaw > math.pi:
+            self.current_yaw -= 2 * math.pi
 
     def timer_callback(self):
         msg = AckermannDriveStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = 'base_link'
+        print(f"Current Yaw: {self.current_yaw}")
         
         if (self.i == 0):
-            msg.drive.steering_angle = 0.0
+            msg.drive.steering_angle = 1.0
             msg.drive.speed = 0.0
             msg.drive.acceleration = 0.0
             print("STOP")
             
-        print(f"Current Yaw: {self.current_yaw}")
         
-        elif (1 < self.i < 11 and self.map_index = 0): # DRIVE STRAIGHT
-            msg.drive.steering_angle = 0.0
+        elif (1 < self.i < 21 and self.map_index == 0): # DRIVE STRAIGHT
+            msg.drive.steering_angle = -0.0525
             msg.drive.speed = 1.0
             msg.drive.acceleration = 0.1
 
-        elif (self.i == 11 and self.map_index = 0): # First Turn
+        elif (self.i == 21 and self.map_index == 0): # First Turn
+            self.turning = True
+            
+        elif (1 < self.i < 11 and self.map_index == 1): # DRIVE STRAIGHT
+            msg.drive.steering_angle = -0.0525
+            msg.drive.speed = 1.0
+            msg.drive.acceleration = 0.1
+
+        elif (self.i == 11 and self.map_index == 1): # Second Turn
             self.turning = True
             
         elif self.turning:
@@ -67,6 +75,8 @@ class AckermannPublisher(Node):
             else:
                 self.turning = False
                 self.map_index += 1
+                self.i = 1
+                self.target_yaw += self.target_yaw
                 print("FInished Turning")
             
         """   
